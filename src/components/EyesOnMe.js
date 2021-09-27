8195c18d736043772a0175455d775d2749c89e8b
Yonatan Kawo <yonas478@gmail.com>
2021-09-27 07:34:05 -0500

    commit tree b7b7b769b467f331e189cc9cce76b798a3d75481

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+const onFucusHandler = (e) =>{
+    e.preventDefault();
+    console.log('Good!');
+}
+const onBlurHandler = (e) => {
+    e.preventDefault();
+    console.log('Hey! Eyes on me!')
+}
+    
+    return (
+        <div>
+        <button onFocus={onFucusHandler} onBlur={onBlurHandler}>Eyes on me</button>
+            
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react'
+
+
+const Keypad = () => {
+    const onChangeHandler = (e) => {
+        e.preventDefault();
+        console.log("Entering password...");
+
+    }
+    return (
+        <div>
+            <input type="password" name="password" onChange={onChangeHandler}/>
+            <label htmlFor="Password"></label>
+        </div>
+    )
+}
+
+export default Keypad
